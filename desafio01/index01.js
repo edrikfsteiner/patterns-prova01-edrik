93e11fa1d4de9e0137b6694fc287c9fe2f0a9a0c
Edrik Fontana Steiner <edrik.2523@alunosatc.edu.br>
2025-09-17 19:18:56 -0300

    Adicionado solução do problemBuilder

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,75 @@
-console.log("Hello, World! 001");
+class Lanche {
+    constructor() {
+      this.pao = false;
+      this.carne = false;
+      this.queijo = false;
+      this.salada = false;
+      this.molho = false;
+    }
+  
+    show() {
+      console.log("Lanche:", {
+        pao: this.pao,
+        carne: this.carne,
+        queijo: this.queijo,
+        salada: this.salada,
+        molho: this.molho
+      });
+    }
+}
+  
+class LancheBuilder {
+    constructor() {
+        this.lanche = new Lanche();
+    }
+
+    setPao(tipo) {
+        this.lanche.pao = tipo;
+        return this;
+    }
+
+    setCarne(tipo) {
+        this.lanche.carne = tipo;
+        return this;
+    }
+
+    setQueijo(tipo) {
+        this.lanche.queijo = tipo;
+        return this;
+    }
+
+    setSalada(tipo) {
+        this.lanche.salada = tipo;
+        return this;
+    }
+
+    setMolho(tipo) {
+        this.lanche.molho = tipo;
+        return this;
+    }
+}
+  
+class LanchoneteDirector {
+    static lancheSimples() {
+      return new LancheBuilder()
+        .setPao(true)
+        .setCarne(true)
+        .build();
+    }
+  
+    static lancheCompleto() {
+      return new LancheBuilder()
+        .setPao(true)
+        .setCarne(true)
+        .setQueijo(true)
+        .setSalada(true)
+        .setMolho(true)
+        .build();
+    }
+}
+
+const lancheSimples = LanchoneteDirector.fazerXSalada();
+const lancheCompleto = LanchoneteDirector.fazerXTudo();
+
+lancheSimples.show();
+lancheCompleto.show();
